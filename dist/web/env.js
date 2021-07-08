e561325b119503f235c35f71b210e6ef61a9cae9
“devops <devops@gmail.com>
2021-07-08 15:29:57 +0530

    added

---
+++ dist/web/env.js
@@ -0,0 +1,10 @@
+(function (window) {
+  window.__env = window.__env || {};
+
+  // API url
+   window.__env.apiHost = 'http://localhost';
+
+  // Whether or not to enable debug mode
+  // Setting this to false will disable console output
+  window.__env.enableDebug = false;
+}(this));
